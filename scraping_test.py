710f81c73a2701c980a228c52c1875494ba0da09
Caleb <calebb2202@gmail.com>
2025-07-02 21:55:00 +0200

    Update scraping_test.py

--- scraping_test.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-
-url = "https://pvpoke.com/battle/1500/abomasnow/abra/11/1-3-2/0-1-4/"
-
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decod('utf-8')
-
-print(html)
